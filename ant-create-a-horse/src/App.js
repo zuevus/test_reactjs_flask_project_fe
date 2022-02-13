57165e547f9e320fab293200cf2cb5c4d612efe9
Yuriy Zuev <Yuriy.Zuev@swtecnn.com>
2022-02-13 12:55:21 +0300

    Initial commit

---
+++ ant-create-a-horse/src/App.js
@@ -0,0 +1,44 @@
+
+import { Button } from 'antd';
+import './App.css';
+
+import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
+       <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
+/* const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App; */
